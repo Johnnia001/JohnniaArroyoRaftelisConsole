a250228c47f3739f96eaacfbcaffd1ae55f1bc0e
johnnia001 <jarroyo.staff@gmail.com>
2024-03-26 14:58:09 -0700

    Console reads and prints txt file.

--- JohnniaArroyoRaftelisConsole/Program.cs
+++ JohnniaArroyoRaftelisConsole/Program.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.IO;
+﻿// using System;
+// using System.IO;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Prompt the user to enter the path to the .txt file
-        Console.WriteLine("/Users/miti/Raftelis/JohnniaArroyoRaftelisConsole/JohnniaArroyoRaftelisConsole/Parcels.txt");
-        string filePath = Console.ReadLine();
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("/Users/miti/Raftelis/JohnniaArroyoRaftelisConsole/JohnniaArroyoRaftelisConsole/Parcels.txt");
+//         string filePath = Console.ReadLine();
 
-        // Check if the file exists
-        if (File.Exists(filePath))
-        {
-            // Read all lines from the file and print them out
-            string[] lines = File.ReadAllLines(filePath);
-            Console.WriteLine("Contents of the file:");
-            foreach (string line in lines)
-            {
-                Console.WriteLine(line);
-            }
-        }
-        else
-        {
-            Console.WriteLine("File not found!");
-        }
+//         if (File.Exists(filePath))
+//         {
+//             string[] lines = File.ReadAllLines(filePath);
+//             Console.WriteLine("Contents of the file:");
+//             foreach (string line in lines)
+//             {
+//                 Console.WriteLine(line);
+//             }
+//         }
+//         else
+//         {
+//             Console.WriteLine("File not found!");
+//         }
 
-        Console.ReadLine(); // Keep the console window open until Enter is pressed
-    }
-}
+//         Console.ReadLine();
+//     }
+// }
